2cc0a021cd7a9f2d683a6031cde4721ece16587c
Explosion-Scratch <explosion.scratch@gmail.com>
2021-07-09 22:53:34 +0000

    stuff

---
+++ commit.bat
@@ -0,0 +1,1 @@
+git stage . && git commit -m "stuff"
---
+++ index.js
@@ -0,0 +1,16 @@
+var Vibrant = require('node-vibrant');
+const express = require("express");
+const app = express();
+
+app.get("/color", query, async (req, res) => {
+    res.json(await Vibrant.from(req.query.q).getPalette())
+})
+
+function query(req, res, next){
+    if (!req.query.q){
+        return res.json({error: true, message: "No query parameter specified."})
+    }
+    return next();
+}
+
+app.listen(3000)
