5e781432b9a18341bc1e77da62e2c346c430e1b8
Explosion-Scratch <explosion.scratch@gmail.com>
2021-07-09 23:23:14 +0000

    stuff

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var Vibrant = require('node-vibrant');
 const express = require("express");
 const app = express();
 
+app.use(express.static(__dirname))
+
 app.get("/color", query, async (req, res) => {
     res.json(await Vibrant.from(req.query.q).getPalette())
 })
