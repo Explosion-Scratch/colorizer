56bb3671c108d9c1bef332a75d1f5ac88837054d
Explosion-Scratch <explosion.scratch@gmail.com>
2021-07-10 12:05:14 +0000

    stuff

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get("/color", query, async (req, res) => {
-  res.json(await Vibrant.from(req.query.q).getPalette());
+  var color;
+  try {
+    color = await Vibrant.from(req.query.q).getPalette();
+  } catch (e) {
+    console.log("There was an error: ", e);
+    console.log(e.message);
+    color = { error: true, message: e.message };
+  }
+  res.json(color);
 });
 app.get("/imgsearch", query, async (req, res) => {
   var gis = require("g-i-s");
